bcea02eac704410851c5f7631638c60521fe4db7
anhtiendzc <tinv65985@gmail.com>
2025-06-22 15:35:27 +0700

    Update app.py

--- kiucchungminh/app.py
+++ kiucchungminh/app.py
@@ -39,3 +39,8 @@ def delete_file(filename):
     else:
         flash(f"Không tìm thấy {filename}")
     return redirect(url_for('index'))
+    if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Lấy PORT do Render cung cấp
+    app.run(host='0.0.0.0', port=port)
+
